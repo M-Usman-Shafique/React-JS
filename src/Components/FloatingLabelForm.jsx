9f8c08e06e7fd4cd48cc7efa4d1c3231483bce6b
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-01-05 12:28:14 +0500

    label float

---
+++ src/Components/FloatingLabelForm.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+export default function FloatingLabelForm() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <form action="#" className="w-full max-w-md space-y-8 px-8 py-6">
+        <div className="text-center font-bold text-3xl">Create New Account</div>
+        <div className="input-box">
+          <input
+            required
+            className="inputs peer"
+            id="username"
+            type="text"
+            placeholder="Username"
+          />
+          <label htmlFor="username" className="labels">
+            Username
+          </label>
+        </div>
+        <div className="input-box">
+          <input
+            required
+            className="inputs peer"
+            id="email"
+            type="email"
+            placeholder="Email Address"
+          />
+          <label htmlFor="email" className="labels">
+            Email Address
+          </label>
+        </div>
+        <div className="input-box">
+          <input
+            required
+            className="inputs peer"
+            id="password"
+            type="password"
+            placeholder="Password"
+          />
+          <label htmlFor="password" className="labels">
+            Password
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-gray-100 text-black px-4 py-2 rounded-md text-lg"
+        >
+          Sign up
+        </button>
+      </form>
+    </div>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Input from "./Components/Input";
+import FloatingLabelForm from "./Components/FloatingLabelForm";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Input />
+    <FloatingLabelForm />
   </React.StrictMode>
 );
